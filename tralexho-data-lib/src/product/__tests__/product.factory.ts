675e871329ea53c3fe654b1867b2d483094f10e0
Nikhilnair48 <nikhil.nair48@gmail.com>
2023-07-05 12:18:53 +0530

    feat: setting up test cases sans DB integration

--- tralexho-data-lib/src/product/__tests__/product.factory.ts
+++ tralexho-data-lib/src/product/__tests__/product.factory.ts
@@ -1,9 +1,9 @@
-import Factory from "factory.ts";
+import * as Factory from "factory.ts";
 import { faker } from "@faker-js/faker";
 import { IProduct } from "../types/product";
 
 export const productFactory = Factory.Sync.makeFactory<IProduct>({
-  id: faker.string.uuid(),
+  id: Factory.each(() => faker.string.uuid()), // Ensures a unique id for each product
   name: faker.string.alpha(),
   location: faker.location.city(),
   owner: faker.person.firstName(),
