c88481357b582a66fdb6944835cb8e82ef1501e4
Nikhilnair48 <nikhil.nair48@gmail.com>
2023-07-10 02:54:26 +0530

    feat: integrated common and updated data-lib

--- tralexho-backend/index.ts
+++ tralexho-backend/index.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import { ProductModel, createConnection } from "tralexho-data-lib/dist";
+import { ProductModel, createConnection } from "tralexho-data-lib";
 import cors from "cors";
 
 const app = express();
@@ -13,58 +13,88 @@ app.use(function (_req, res, next) {
 const port = 3001;
 createConnection();
 
-app.get("/products", async (req: Request, res: Response) => {
-  const products = await ProductModel.find().exec();
-  return res.status(200).json(products);
+app.get("/products", async (_req: Request, res: Response) => {
+  try {
+    const products = await ProductModel.find().exec();
+    return res.status(200).json(products);
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error fetching products.",
+    });
+  }
 });
 
 app.get("/product/:productId", async (req: Request, res: Response) => {
-  const { productId } = req.params;
-  const product = await ProductModel.find({ _id: productId }).exec();
-  return res.status(200).json(product);
+  try {
+    const { productId } = req.params;
+    const product = await ProductModel.find({ _id: productId }).exec();
+    return res.status(200).json(product);
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error fetching product.",
+    });
+  }
 });
 
 app.post("/product", async (req: Request, res: Response) => {
-  const { name, location, owner, quantityInGrams } = req.body;
-  const product = await ProductModel.create({
-    name,
-    location,
-    owner,
-    quantityInGrams,
-  });
-  return res.status(200).json(product);
-});
-
-app.put("/product/:productId", async (req: Request, res: Response) => {
-  const { productId } = req.params;
-  const { name, location, owner, quantityInGrams } = req.body;
-  const updatedPerson = await ProductModel.findByIdAndUpdate(
-    {
-      _id: productId,
-    },
-    {
+  try {
+    const { name, location, owner, quantityInGrams } = req.body;
+    const product = await ProductModel.create({
       name,
       location,
       owner,
       quantityInGrams,
+    });
+    return res.status(200).json(product);
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error creating product.",
+    });
+  }
+});
+
+app.put("/product/:productId", async (req: Request, res: Response) => {
+  try {
+    const { productId } = req.params;
+    const { name, location, owner, quantityInGrams } = req.body;
+    const updatedPerson = await ProductModel.findByIdAndUpdate(
+      {
+        _id: productId,
+      },
+      {
+        name,
+        location,
+        owner,
+        quantityInGrams,
+      }
+    );
+    if (updatedPerson) {
+      res.json(updatedPerson);
+    } else {
+      res.status(404).json({ error: "Product not found" });
     }
-  );
-  if (updatedPerson) {
-    res.json(updatedPerson);
-  } else {
-    res.status(404).json({ error: "Product not found" });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error updating product.",
+    });
   }
 });
 
 app.delete("/product/:productId", async (req: Request, res: Response) => {
-  const { productId } = req.params;
-  const deletedPerson = await ProductModel.findByIdAndDelete({
-    _id: productId,
-  }).exec();
-  if (deletedPerson) {
-    res.json({ message: "Product deleted" });
-  } else {
-    res.status(404).json({ error: "Product not found" });
+  try {
+    const { productId } = req.params;
+    const deletedPerson = await ProductModel.findByIdAndDelete({
+      _id: productId,
+    }).exec();
+    if (deletedPerson) {
+      res.json({ message: "Product deleted" });
+    } else {
+      res.status(404).json({ error: "Product not found" });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error updating product.",
+    });
   }
 });
 
